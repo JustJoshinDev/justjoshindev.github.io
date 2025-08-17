6ea5f65da0cf86001c26d223ab734f874bb78138
JustJoshinDev <suppers789@gmail.com>
2025-08-16 20:16:56 -0500

    Update Spotify ngrok URL

--- spotify_url.js
+++ spotify_url.js
@@ -1,1 +1,1 @@
-const SPOTIFY_JSON_URL = "NgrokTunnel: "https://4ca381422c72.ngrok-free.app" -> "http://127.0.0.1:5000"/current_song.json";
+const SPOTIFY_JSON_URL = "NgrokTunnel: "https://9b6288076e15.ngrok-free.app" -> "http://127.0.0.1:5000"/current_song.json";
